620c39372b80b462ac892966b6075436b90817b2
imdotnhsang <im.nhsang.dev@gamil.com>
2024-03-15 00:47:34 +0700

    chore(app): test snapshot

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,5 +4,5 @@ import { memo } from 'react';
 interface IButtonProps {}
 
 export const Button: FC<IButtonProps> = memo(function Button() {
-  return <div>Button</div>;
+  return <div>123</div>;
 });
