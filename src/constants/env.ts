a00c333fb09e4a7765b1a94886918c7b126318d1
imdotnhsang <im.nhsang.dev@gamil.com>
2024-03-15 00:08:28 +0700

    chore(app): set up vitest github action

---
+++ src/constants/env.ts
@@ -0,0 +1,3 @@
+export const CEnv = Object.freeze({
+  MEDIA_DOMAIN: process.env.NEXT_PUBLIC_MEDIA_DOMAIN
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@
  * @purpose
  * - Perform common or generic operations that are not tied to any specific part of the program
  */
+export * from './media';
