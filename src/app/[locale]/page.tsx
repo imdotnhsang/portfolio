5f7fcd5497f0c680154e97fb5b30efdc7dfc680f
imdotnhsang <im.nhsang.dev@gamil.com>
2024-03-21 21:27:03 +0700

    chore(app): config colors, border, rounded

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,11 @@ import { useTranslations } from 'next-intl';
 const App = () => {
   const t = useTranslations('app');
 
-  return <div>app page {t('title')}</div>;
+  return (
+    <div className='border-1 border-cyan size-10 bg-purple'>
+      app page {t('title')}
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,124 @@
 import { createThemes } from 'tw-colors';
 
+const widthSizes = {
+  0.5: '0.125rem',
+  1: '0.25rem',
+  1.5: '0.375rem',
+  2: '0.5rem',
+  2.5: '0.625rem',
+  3: '0.75rem',
+  3.5: '0.875rem',
+  4: '1rem',
+  4.5: '1.125rem',
+  5: '1.25rem',
+  5.5: '1.375rem',
+  6: '1.5rem',
+  6.5: '1.625rem',
+  7: '1.75rem',
+  7.5: '1.875rem',
+  8: '2rem',
+  8.5: '2.125rem',
+  9: '2.25rem',
+  9.5: '2.375rem',
+  10: '2.5rem'
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {}
+    extend: {
+      borderRadius: widthSizes,
+      borderWidth: widthSizes
+    }
   },
   plugins: [
     createThemes({
       light: {
-        black: '#01080E',
-        white: '#FFFFFF',
+        line: '#1E2D3D',
+        'black-strong': '#01080E',
+        'black-subtle': '#011221',
+        'black-slight': '#011627',
         gray: '#607B96',
-        sunset: '#FEA55F',
+        blue: '#3C9D93',
+        violet: '#4D5BCE',
+        white: '#FFFFFF',
+        brown: '#FEA55F',
         cyan: '#43D9AD',
-        brown: '#E99287',
-        violet: '#C98BDF',
-        green: '#3C9D93',
-        purple: '#4D5BCE'
+        pink: '#E99287',
+        purple: '#C98BDF',
+
+        base: {
+          line: '#1E2D3D',
+          'black-strong': '#01080E',
+          'black-subtle': '#011221',
+          'black-slight': '#011627',
+          gray: '#607B96',
+          blue: '#3C9D93',
+          violet: '#4D5BCE',
+          white: '#FFFFFF',
+          brown: '#FEA55F',
+          cyan: '#43D9AD',
+          pink: '#E99287',
+          purple: '#C98BDF'
+        },
+        'base-d': {
+          line: '#1E2D3D',
+          'black-strong': '#01080E',
+          'black-subtle': '#011221',
+          'black-slight': '#011627',
+          gray: '#607B96',
+          blue: '#3C9D93',
+          violet: '#4D5BCE',
+          white: '#FFFFFF',
+          brown: '#FEA55F',
+          cyan: '#43D9AD',
+          pink: '#E99287',
+          purple: '#C98BDF'
+        }
       },
       dark: {
-        black: '#01080E',
-        white: '#FFFFFF',
+        line: '#1E2D3D',
+        'black-strong': '#01080E',
+        'black-subtle': '#011221',
+        'black-slight': '#011627',
         gray: '#607B96',
-        sunset: '#FEA55F',
+        blue: '#3C9D93',
+        violet: '#4D5BCE',
+        white: '#FFFFFF',
+        brown: '#FEA55F',
         cyan: '#43D9AD',
-        brown: '#E99287',
-        violet: '#C98BDF',
-        green: '#3C9D93',
-        purple: '#4D5BCE'
+        pink: '#E99287',
+        purple: '#C98BDF',
+
+        base: {
+          line: '#1E2D3D',
+          'black-strong': '#01080E',
+          'black-subtle': '#011221',
+          'black-slight': '#011627',
+          gray: '#607B96',
+          blue: '#3C9D93',
+          violet: '#4D5BCE',
+          white: '#FFFFFF',
+          brown: '#FEA55F',
+          cyan: '#43D9AD',
+          pink: '#E99287',
+          purple: '#C98BDF'
+        },
+        'base-d': {
+          line: '#1E2D3D',
+          'black-strong': '#01080E',
+          'black-subtle': '#011221',
+          'black-slight': '#011627',
+          gray: '#607B96',
+          blue: '#3C9D93',
+          violet: '#4D5BCE',
+          white: '#FFFFFF',
+          brown: '#FEA55F',
+          cyan: '#43D9AD',
+          pink: '#E99287',
+          purple: '#C98BDF'
+        }
       }
     })
   ]
