a2f8c1a0c719cca99a60d6e4f886adb8d76e7310
imdotnhsang <im.nhsang.dev@gamil.com>
2024-04-18 16:35:26 +0700

    feat(app): fix tsc svg

---
+++ types/image.d.ts
@@ -0,0 +1,6 @@
+import type { FunctionComponent, SVGAttributes } from 'react';
+
+declare module '*.svg' {
+  const content: FunctionComponent<SVGAttributes<SVGElement>>;
+  export default content;
+}
