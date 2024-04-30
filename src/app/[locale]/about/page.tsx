db32d39fc00eb8f47e8f7cdbc13adcc2b3ebc4df
imdotnhsang <im.nhsang.dev@gamil.com>
2024-04-30 11:37:45 +0700

    feat(app): add route loder

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -2,7 +2,6 @@ import { unstable_setRequestLocale } from 'next-intl/server';
 
 import type { FC } from 'react';
 
-// import { cn } from '@/services';
 import { UnderConstruction } from '@/partials';
 
 import type { IPageProps } from '@/interfaces';
@@ -13,24 +12,6 @@ const AboutPage: FC<IAboutPageProps> = ({ params: { locale } }) => {
   unstable_setRequestLocale(locale);
 
   return <UnderConstruction />;
-
-  // return (
-  //   <div className='center absolute left-0 top-0 z-2 h-full w-full'>
-  //     {Array.from({ length: 6 }).map((_, index) => (
-  //       <div
-  //         key={index}
-  //         className={cn(
-  //           'absolute top-0 h-full w-1/6 bg-color-secondary transition-transform duration-200 bg-color-quaternary -translate-y-full'
-  //         )}
-  //         style={{
-  //           transitionDelay: `${index * 75}ms`,
-  //           left: `${((index * 1) / 6) * 100}%`,
-  //           // transform: 'translateY(0)'
-  //         }}
-  //       />
-  //     ))}
-  //   </div>
-  // );
 };
 
 export default AboutPage;
--- src/app/[locale]/blog/loading.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from 'react';
-
-const AboutLoading: FC = () => {
-  return <div>about loading</div>;
-};
-
-export default AboutLoading;
